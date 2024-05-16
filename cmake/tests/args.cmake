f20c8d8b3c156f8f1d78c7404fd5dc25a53405d0
Joo Hyoung Cha <aoikazto@naver.com>
2024-05-16 17:52:25 +0900

    integrate gtest.

--- cmake/tests/args.cmake
+++ cmake/tests/args.cmake
@@ -3,7 +3,6 @@ add_executable(TestProgram tests/args/import.cpp)
 
 target_link_libraries(TestProgram PRIVATE 
             ${PROJ_NAME}
-            GTest::gtest GTest::gtest_main GTest::gmock GTest::gmock_main)
+            GTest::gtest GTest::gtest_main)
 
-add_test(AllTestsInArgument TestProgram)
-
+add_test(Test1 TestProgram)
